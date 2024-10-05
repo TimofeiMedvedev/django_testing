f946c59c780d085776578eed926255db5ef4e54e
Timofei Medvedev <medtv78@gmail.com>
2024-10-05 16:40:38 +0300

    fr2

--- ya_note/notes/tests/common.py
+++ ya_note/notes/tests/common.py
@@ -1,11 +1,8 @@
 from collections import namedtuple
-from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
-
-from notes.models import Note
 
 User = get_user_model()
 
@@ -37,37 +34,16 @@ URL_REVERSE = URL_NAME(
     reverse('notes:detail', args=(SLUG,)),
     reverse('notes:edit', args=(SLUG,)),
     reverse('notes:delete', args=(SLUG,)),
-)   
-
-
-
-
-LOGIN_URL = reverse('users:login')
-ADD_URL = reverse('notes:add')
-SUCCES_URL = reverse('notes:success')
-EDIT_URL = reverse('notes:edit', args=(SLUG,))
-DELETE_URL = reverse('notes:delete', args=(SLUG,))
-
+)
 
 
 class BaseTest(TestCase):
-    
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Автор')
         cls.author_client = Client()
-        cls.author_client.force_login(cls.author) 
+        cls.author_client.force_login(cls.author)
 
         cls.not_author = User.objects.create(username='Не автор')
         cls.not_author_client = Client()
         cls.not_author_client.force_login(cls.not_author)
-
-
-        
-        # cls.notes = Note.objects.create(
-        #     title='Заголовок',
-        #     text='Текст',
-        #     slug=SLUG,
-        #     author=cls.author)
-        
-    
